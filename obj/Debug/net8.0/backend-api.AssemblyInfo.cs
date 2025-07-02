5035cd4ef81090411c3ebd40ac456add9f39c9f0
Daniel Abreu <Danielabreu2310@hotmail.com>
2025-07-02 17:41:21 -0400

    initial commit

--- obj/Debug/net8.0/backend-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82e68dd1af2b732e8bec08ef12b268a96470e9a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
